95ac187f94f54a728de0d91c39c9bf2b5bbfb897
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-03-21 16:44:32 +0100

    5.4.6 exercise 2

---
+++ exercises_5_4_6/exercise_2.py
@@ -0,0 +1,12 @@
+def add_fruit(inventory, fruit, quantity=0):
+    inventory[fruit] = inventory.get(fruit, 0) + quantity
+    return inventory
+
+
+# Make these tests work...
+new_inventory = {}
+add_fruit(new_inventory, "strawberries", 10)
+print("strawberries" in new_inventory)
+print(new_inventory["strawberries"] == 10)
+add_fruit(new_inventory, "strawberries", 25)
+print(new_inventory["strawberries"] == 35)
