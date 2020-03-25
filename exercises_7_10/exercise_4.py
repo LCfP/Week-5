4410bf1279dff828e438ec8a2d0bce38d4a17d79
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-03-25 15:07:09 +0100

    7.10 exercise 4 (b)

--- exercises_7_10/exercise_4.py
+++ exercises_7_10/exercise_4.py
@@ -3,4 +3,10 @@ with open("exercise_3_output.txt") as input_file:
 
 with open("exercise_4_output_a.txt", "w") as output_file:
     for line in lines:
-        output_file.write(line[5:])     # skip the first 5 characters, the four numbers and the space
+        output_file.write(line[5:])             # skip the first 5 characters, the four numbers and the space
+
+with open("exercise_4_output_b.txt", "w") as output_file:
+    for line in lines:
+        line = " ".join(line.split(" ")[1:])    # skip the first "word" in each line
+                                                # (where word is defined as something followed by a space)
+        output_file.write(line)
